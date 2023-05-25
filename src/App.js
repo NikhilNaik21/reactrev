1e9071e98e71df338ac1bced1004a961239213e4
my name <my@example.com>
2023-05-25 17:02:33 +0530

    React Container Access

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import Login from "./Login";
 import './App.css';
-
 function App() {
+  let prn =43;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      <h3>My Name is Nikhil Naik</h3>
+      <Login prns={prn}></Login>
     </div>
   );
 }
---
+++ src/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+ function Login(props)
+{
+    return (
+        <div>
+             Login 
+            <h3>MY PRN is : {props.prns}</h3>
+        </div>
+    )
+}
+export default Login;
